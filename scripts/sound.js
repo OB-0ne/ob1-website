c5d5849b4438a78d40952f95150e930b0f192303
OB-0ne <omkarbhatt8@gmail.com>
2024-08-02 16:22:41 -0400

    netter beep boop

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,8 +1,9 @@
 let osc, envelope, oldX = 0, oldY = 0;
 let note = 0;
-let frame_release = 35;
+let frame_release = 20;
 let idle = 0;
 let i = 0;
+let arp_num = 0;
 
 let muteButton, muteState;
 
@@ -19,6 +20,14 @@ function setup() {
     // generate all synth variables
     d_bass = new drum_bass();
     d_snare = new drum_snare();
+    // i_sparkles = new sprinkle_clouds();
+
+    // TEMP CODE
+
+    d_bass_sets = [[1, 0, 0, 0],[1, 0, 1, 0],[1, 0, 1, 1]];
+    d_bass_on = d_bass_sets[0];
+    d_snare_sets = [[0, 1, 0, 1],[0, 1, 1, 1]];
+    d_snare_on = d_bass_sets[0];
 
 }
 
@@ -27,36 +36,85 @@ function draw() {
 
     if(audio_context.state=='running'){
 
-        d_bass_sets = [1, 0, 1, 0]
-        d_bass_on = d_bass_sets[0]
-        d_snare_sets = [[0, 1, 0, 1],[0, 1, 1, 1]]
-        d_snare_on = d_bass_sets[0]
-    
+        beat = int(frameCount / frame_release);
+
         if (frameCount % frame_release === 0 || frameCount === 1) {
     
             //envelope.play(osc, 0, 10);
             if(d_bass_on[i]){
                 d_bass.play_me();
             }
-            if(Math.random()>0.75){
-                temp = 1;
-            }
-            else{
-                temp = 0;
-            }
             if(d_snare_on[i]){
                 d_snare.play_me();
             }
 
+            // if(beat >= 48){
+            //     if(i == 0 || i == 2){
+            //         i_sparkles.play_me(arp_num, i);
+            //     }
+            //     else{
+            //         if(beat>=80){
+            //             if(Math.random() > 0.7){
+            //                 i_sparkles.play_me(arp_num, i);
+            //             }
+            //         }
+            //         if(beat>=120){
+            //             if(Math.random() > 0.3){
+            //                 i_sparkles.play_me(arp_num, i);
+            //             }
+            //         }
+            //     }                    
+            // }
+
             // create a manual beat tracker
             i = i + 1;
+            // reset at 4th beat
             if(i == 4){
-                // reset at 4th beat
+                
                 i = 0;
                 
                 // reset to a random snare pattern
+                if(Math.random() > 0.7){
+                    d_snare_on = d_snare_sets[1];
+                }
+                else{
+                    d_snare_on = d_snare_sets[0];
+                }
+                
+                // rset bass pattern
+                if(beat % 8*4 == 0){
+                    d_bass_on = d_bass_sets[1];
+                }
+                else{
+                    if(beat % 10*4 == 0){
+                        d_bass_on = d_bass_sets[2];
+                    }
+                    else{
+                        if(beat % 12*4 == 0){
+                            d_bass_on = d_bass_sets[0];
+                        }  
+                    }                        
+                }
+                
+
+                // reset to arpegiator
+                if(beat>64){
+                    if(Math.random() > 0.92){
+                        arp_num = 3
+                    }
+                    if(Math.random() > 0.84){
+                        arp_num = 2
+                    }
+                    if(Math.random() > 0.75){
+                        arp_num = 1;
+                    }
+                    else{
+                        arp_num = 0;
+                    }
+                }
                 
             }
+            console.log('Beat: ',beat, d_bass_on);
         }
     }   
 
--- scripts/sound_synths.js
+++ scripts/sound_synths.js
@@ -7,15 +7,15 @@ class drum_bass {
     constructor(){
         this.osc.start();
         this.osc.amp(0.6);
-        this.osc.freq(55);
+        this.osc.freq(58);
     
         // Instantiate the envelope
-        this.env.setADSR(0.001, 0.6, 0.4, 1);
+        this.env.setADSR(0.001, 0.6, 0.4, 0.2);
         this.env.setRange(1, 0);
 
         // add some reverb
-        this.reverb.process(this.osc, 0.1, 1);
-        this.reverb.drywet(.8);
+        this.reverb.process(this.osc, 0.1, 0.2);
+        this.reverb.drywet(.4);
     }
 
     /**
@@ -34,22 +34,59 @@ class drum_snare {
 
     constructor(){
         this.osc.start();
-        this.osc.amp(0.6);
-        this.osc.freq(220);        
+        this.osc.amp(0.15);
+        this.osc.freq(138);        
     
         // Instantiate the envelope
-        this.env.setADSR(0.001, 0.1, 0.6, 0);
-        this.env.setRange(0.8, 0);
+        this.env.setADSR(0.001, 0.1, 0.1, 0.1,0);
+        this.env.setRange(0.6, 0.2, 0.2, 0);
 
         // add some reverb
-        this.reverb.process(this.osc, 10, 10);
-        this.reverb.drywet(.8);
+        this.reverb.process(this.osc, .2, 0.2);
+        this.reverb.drywet(.4);
     }
 
     /**
      * This plays the synth when triggered
      */
     play_me() {
+        this.env.play(this.osc);
+        this.osc.freq(130 + Math.random()*16); 
+    }
+}
+
+class sprinkle_clouds {
+    
+    osc = new p5.SinOsc();
+    env = new p5.Envelope();
+    reverb = new p5.Reverb();
+    delay = new p5.Delay();
+
+    arps = [[0,1,4,5],[0,2,4,5],[2,4,5,5],[2,7,4,5]];
+    root_note_midi = 70; // A#4
+    
+    constructor(){
+        this.osc.start();
+        this.osc.amp(0.6);
+        this.osc.freq(0);        
+    
+        // Instantiate the envelope
+        this.env.setADSR(0.001, 0.1, 0.4, 0);
+        this.env.setRange(0.2, 0);
+
+        this.delay.process(this.osc, 0.42, 0.55, 1200);
+        this.delay.drywet(0.6);
+
+        // add some reverb
+        this.reverb.process(this.osc, 1, .2);
+        this.reverb.drywet(.6);
+    }
+
+    /**
+     * This plays the synth when triggered
+     */
+    play_me(arp_num, curr_beat) {
         this.env.play(this.osc,0,0);
+        this.osc.freq(midiToFreq(this.root_note_midi + this.arps[arp_num][curr_beat])); 
     }
 }
