ac4a03733ebd19d6b2c2721d72cc2fd7a34b6294
OB-0ne <omkarbhatt8@gmail.com>
2024-10-17 01:08:55 +0530

    updated trip data with routes
    - python preprocessing uses the routing API to get all routes between points
    - it is able to understand driving and hiking to an extent (not fully)
    - polylines with customized points make it easy to to snapped routes for the map

--- scripts/geomap.js
+++ scripts/geomap.js
@@ -16,56 +16,18 @@ function draw_map(){
         var marker = L.marker([d.latitude, d.longitude]).addTo(map); // Los Angeles
         marker.bindPopup(d.description);
       }
-      else if (d.type = "Moving"){
-        if (i > 100 && i < 110){
-          var temp = [];
-          temp.push(parseFloat(d.latitude));
-          temp.push(parseFloat(d.longitude));
-          routeCoordinates.push(temp);
-        } 
-        i = i + 1;
-      }        
+      else {
+        var temp = [];
+        temp.push(parseFloat(d.latitude));
+        temp.push(parseFloat(d.longitude));
+        routeCoordinates.push(temp);
+      }
     });
-    // console.log(routeCoordinates);
-    // var route = L.Routing.control({
-    //   waypoints: routeCoordinates,
-    //   routeWhileDragging: true,
-    //   show: false,
-    //   lineOptions: {
-    //     styles: [{ color: 'blue', weight: 5, opacity: 0.7 }] // Customize route style
-    //   },
-    //   createMarker: function() { return null; }
-    // }).addTo(map);
-
+    
+    L.polyline(routeCoordinates, {color: 'blue'}).addTo(map);
   });
 
-  
-
-  // var marker = L.marker([34.0522, -118.2437]).addTo(map); // Los Angeles
-  // marker.bindPopup("<b>Los Angeles</b><br>Had an amazing time here!");
-
-  // var marker = L.marker([36.1699, -115.1398]).addTo(map); // Las Vegas
-  // marker.bindPopup("<b>Las Vegas</b><br><img src='https://media.nomadicmatt.com/2023/vegasthings1.jpeg' width='100' alt='Vegas'><br>This was an awesome stop!").openPopup();
-
-  var routeCoordinates = [
-      [37.7749, -122.4194], // San Francisco
-      [36.1699, -115.1398], // Las Vegas
-      [34.0522, -118.2437], // Los Angeles
-      [38.580786, -107.717514]
-  ];
-
-  var route = L.Routing.control({
-    waypoints: routeCoordinates,
-    routeWhileDragging: true,
-    show: false,
-    lineOptions: {
-      styles: [{ color: 'blue', weight: 5, opacity: 0.7 }] // Customize route style
-    },
-    createMarker: function() { return null; }
-  }).addTo(map);
-    route.hide();
-
-  // Zoom the map to fit the route
-  // map.fitBounds(route.getBounds());
+  map.fitBounds(L.latLngBounds([44.24, -124.67],[30.33, -102.70]));
+  map.setMaxBounds(map.getBounds());
 
 }
