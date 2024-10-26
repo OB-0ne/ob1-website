a09e6df8e3b5cc2b01956de00039b8b07d81537e
OB-0ne <omkarbhatt8@gmail.com>
2024-10-27 01:17:38 +0530

    markers for images
    - added image information data
    - markers are created for all the images

--- scripts/geomap.js
+++ scripts/geomap.js
@@ -10,10 +10,11 @@ function draw_map(){
   var routeCoordinates = [];
   i = 0;
 
+  // Generate the point sof interest from the trip GPS info
   d3.csv("data/trip_timeline.csv", function(err, data) {
     data.forEach(function(d) {
       if (d.type == "Visit"){
-        var marker = L.marker([d.latitude, d.longitude]).addTo(map); // Los Angeles
+        var marker = L.marker([d.latitude, d.longitude]).addTo(map);
         marker.bindPopup(d.description);
       }
       else {
@@ -25,6 +26,14 @@ function draw_map(){
     });
     
     L.polyline(routeCoordinates, {color: 'blue'}).addTo(map);
+  });
+
+  // Generate the points of interest from trip images
+  d3.csv("data/trip_imageinfo.csv", function(err, img_data) {
+    img_data.forEach(function(d) {
+      var marker = L.marker([d.latitude, d.longitude]).addTo(map); 
+      // marker.attr('class','image_marker');
+    });
   });
 
   map.fitBounds(L.latLngBounds([44.24, -124.67],[30.33, -102.70]));
