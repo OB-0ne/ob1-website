dfde55c2241d141d7a9f559f24d315f3177d94a1
OB-0ne <omkarbhatt8@gmail.com>
2024-10-27 17:00:01 +0530

    updated image pop-ups
    - added custom icons for markers
    - updated pop-up to show the image
    - updated css to auto adjust height and width as per the image

--- scripts/geomap.js
+++ scripts/geomap.js
@@ -1,6 +1,17 @@
+
 function draw_map(){
 
   var map = L.map('map').setView([37.7749, -122.4194], 5); // Default to San Francisco
+
+  var imageMarker = new L.icon({
+    iconUrl: 'icons/map/image-marker.png',
+    iconSize: [75,75]
+  });
+
+  var poiMarker = new L.icon({
+    iconUrl: 'icons/map/poi-marker.png',
+    iconSize: [75,75]
+  });
 
   // Add a tile layer (you can change to other providers if needed)
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,8 +25,8 @@ function draw_map(){
   d3.csv("data/trip_timeline.csv", function(err, data) {
     data.forEach(function(d) {
       if (d.type == "Visit"){
-        var marker = L.marker([d.latitude, d.longitude]).addTo(map);
-        marker.bindPopup(d.description);
+        // var marker = L.marker([d.latitude, d.longitude], {icon: poiMarker, title: d.description}).addTo(map);
+        // marker.bindTooltip(d.description);
       }
       else {
         var temp = [];
@@ -31,8 +42,13 @@ function draw_map(){
   // Generate the points of interest from trip images
   d3.csv("data/trip_imageinfo.csv", function(err, img_data) {
     img_data.forEach(function(d) {
-      var marker = L.marker([d.latitude, d.longitude]).addTo(map); 
-      // marker.attr('class','image_marker');
+      var marker = L.marker([d.latitude, d.longitude], {icon: imageMarker, class: 'abc'}).addTo(map); 
+      const popupContent = `
+          <div>
+            <img src="${"data/image_content/trip/" + d.filename}" class="geomap_marker_trip_image" />
+          </div>
+        `;
+        marker.bindPopup(popupContent);
     });
   });
 
