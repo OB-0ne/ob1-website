644581b659766ec8135ec1056aebc1524ed57541
Omkar Bhatt <47946985+OB-0ne@users.noreply.github.com>
2024-10-29 01:48:21 +0530

    Interactive GroMap on Leaflet to showcase the trip information (#6)
    * first commit for the map
    
    - added a new page for the custom map
    - added script to load maps via leaflet
    - added examples to add markers
    - added examples to add routing from lat/long points
    
    * csv to map generation with some issues
    
    - added d3 library to the map
    - map generate all markers easily
    - all route points are being collected but are not being routed/drawn on the map
    - I may have overshot the demo server limits with this one
    
    * updated trip data with routes
    
    - python preprocessing uses the routing API to get all routes between points
    - it is able to understand driving and hiking to an extent (not fully)
    - polylines with customized points make it easy to to snapped routes for the map
    
    * markers for images
    
    - added image information data
    - markers are created for all the images
    
    * updated image pop-ups
    
    - added custom icons for markers
    - updated pop-up to show the image
    - updated css to auto adjust height and width as per the image
    
    * added map control for visits and images
    
    - added layer groups to combine all visit and image markers
    - added default selections while loading the map
    - checkboxes hide/unhide the markers
    
    * info pop-up added
    
    - pop-up divs added
    - any buttons needed for controlling pop-0up added with functionality
    
    * map completed for publishing with updated home page
    
    - updated all page footer to add leaflet info
    - updated UI for geomap pop-up
    - added info on the geomap pop-up
    - updated the map details on the project tab as well as the home tab
    - updated trip data with updated visits and their description
    - updated home page description for what's working
    - updated the beep boops to be more frequent and fixed their reverb effects on home page

---
+++ scripts/geomap.js
@@ -0,0 +1,96 @@
+
+function draw_map(){
+
+  // Add a tile layer (you can change to other providers if needed)
+  var def_Map = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  });
+
+  // make layer grousp for the markers - visist and images
+  var imgMark_Layer = L.featureGroup();
+  var visitMark_Layer = L.featureGroup();
+
+  // create dictionaries for baseMaps - in this case only the default map
+  // this is a neede variable while making a layer control
+  var baseMaps = {
+    "Deafult Map": def_Map
+  }
+  // create a dictionary for different overlays - these will appear as checkbox
+  var overlays = {
+    "Visits": visitMark_Layer,
+    "Photos": imgMark_Layer
+  };
+
+  // initiate the map and select default map tile and selected checkboxes
+  var map = L.map('map',{
+    zoom: 10,
+    layers: [def_Map, visitMark_Layer, imgMark_Layer]
+  });
+
+  // initialize the control which can be interacted with
+  var layerControl = L.control.layers(baseMaps,overlays).addTo(map);
+
+
+  // initiate the icons which will be used for the markers
+  var imageMarker = new L.icon({
+    iconUrl: 'icons/map/image-marker.png',
+    iconSize: [60,60]
+  });
+
+  var poiMarker = new L.icon({
+    iconUrl: 'icons/map/poi-marker.png',
+    iconSize: [60,60]
+  });  
+
+  // a list to store all coordinates which can be converted to a polyline eventually
+  var routeCoordinates = [];
+  i = 0;
+
+  // Generate the points of interest from the trip GPS info
+  d3.csv("data/trip_timeline.csv", function(err, data) {
+    data.forEach(function(d) {
+      if (d.type == "Visit"){
+        var marker = L.marker([d.latitude, d.longitude], {icon: poiMarker, title: d.description}).addTo(visitMark_Layer);
+        marker.bindTooltip(d.description);
+      }
+      else {
+        var temp = [];
+        temp.push(parseFloat(d.latitude));
+        temp.push(parseFloat(d.longitude));
+        routeCoordinates.push(temp);
+      }
+    });
+    
+    var trip_line = L.polyline(routeCoordinates, {color: 'blue', className: 'day0'}).addTo(map);
+    
+  });
+
+  // Generate the points of interest from trip images
+  d3.csv("data/trip_imageinfo.csv", function(err, img_data) {
+    img_data.forEach(function(d) {
+      var marker = L.marker([d.latitude, d.longitude], {icon: imageMarker, class: 'abc'}).addTo(imgMark_Layer); 
+      const popupContent = `
+          <div>
+            <img src="${"data/image_content/trip/" + d.filename}" class="geomap_marker_trip_image" />
+          </div>
+        `;
+        marker.bindPopup(popupContent);
+    });
+  });
+
+  // set default settings for the map including bounds, active layers
+  map.fitBounds(L.latLngBounds([44.24, -124.67],[30.33, -102.70]));
+  map.setMaxBounds(map.getBounds());
+
+}
+
+// Toggle popup visibility
+function toggleGeomapInfoPopup() {
+  const infoPopup = document.getElementById('info-popup');
+  infoPopup.classList.toggle('hidden');
+}
+
+// Show popup on page load
+window.onload = function() {
+  document.getElementById('info-popup').classList.remove('hidden');
+};
